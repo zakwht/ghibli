a21f716087ff02ffae9fd78e7c4fdfdf683e7a41
Zak White <zakwht@gmail.com>
2022-12-29 20:41:18 -0800

    Update image loading

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,21 @@ const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
   window.open(`https://google.com/search?q=${URI}`, "_self");
 };
 
-const still = stills[Math.floor(Math.random() * stills.length)];
+const randomStill = () => stills[Math.floor(Math.random() * stills.length)];
+
+const getStill = () => {
+  const stored = localStorage.getItem("ghibli-extension-still")
+  if (stored) try {
+    return JSON.parse(stored)
+  } catch(e) { return randomStill() }
+  else return randomStill();
+}
+
+const still = getStill()
+// preload next image
+const nextStill = randomStill();
+new Image().src = nextStill.url;
+localStorage.setItem("ghibli-extension-still", JSON.stringify(nextStill))
 
 export const App = () => {
   return (
@@ -15,6 +29,7 @@ export const App = () => {
       style={{
         backgroundImage: `url('${still.url}')`
       }}
+      data-alt={still.alt}
     >
       <input placeholder="Search Google or type a URL" autoCorrect="false" spellCheck="false" onKeyDown={handleKeyDown} />
       <footer>
