beafeb642df3aa944c4e2240fcbd15c9d3b736f0
Zak White <zakwht@gmail.com>
2022-12-29 20:42:28 -0800

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,21 @@ const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
 const randomStill = () => stills[Math.floor(Math.random() * stills.length)];
 
 const getStill = () => {
-  const stored = localStorage.getItem("ghibli-extension-still")
-  if (stored) try {
-    return JSON.parse(stored)
-  } catch(e) { return randomStill() }
+  const stored = localStorage.getItem("ghibli-extension-still");
+  if (stored)
+    try {
+      return JSON.parse(stored);
+    } catch (e) {
+      return randomStill();
+    }
   else return randomStill();
-}
+};
 
-const still = getStill()
+const still = getStill();
 // preload next image
 const nextStill = randomStill();
 new Image().src = nextStill.url;
-localStorage.setItem("ghibli-extension-still", JSON.stringify(nextStill))
+localStorage.setItem("ghibli-extension-still", JSON.stringify(nextStill));
 
 export const App = () => {
   return (
@@ -31,7 +34,12 @@ export const App = () => {
       }}
       data-alt={still.alt}
     >
-      <input placeholder="Search Google or type a URL" autoCorrect="false" spellCheck="false" onKeyDown={handleKeyDown} />
+      <input
+        placeholder="Search Google or type a URL"
+        autoCorrect="false"
+        spellCheck="false"
+        onKeyDown={handleKeyDown}
+      />
       <footer>
         <span>{still.title}</span>
         <span>
