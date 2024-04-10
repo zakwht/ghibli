f763aa54b778e5be254c4bd465ee0198cedff330
Zak White <zakwht@gmail.com>
2024-04-10 16:44:34 -0700

    add topSites sample data for dev mode

--- src/TopSites.tsx
+++ src/TopSites.tsx
@@ -3,50 +3,37 @@ import { useState, useEffect } from "react";
 interface TopSiteURL {
   title: string;
   url: string;
+  src?: string;
 }
 
-// let sample = [
-//   {
-//       "title": "Inbox (236) - zakerywhite.1@gmail.com - Gmail",
-//       "url": "https://mail.google.com/"
-//   },
-//   {
-//       "title": "zakwht (Zak)",
-//       "url": "https://github.com/zakwht"
-//   },
-//   {
-//       "title": "Coast Capital Savings - Online Banking",
-//       "url": "https://my.coastcapitalsavings.com/OnlineBanking/"
-//   },
-//   {
-//       "title": "Albums",
-//       "url": "http://localhost:3000/"
-//   },
-//   {
-//       "title": "Notion – The all-in-one workspace for your notes, tasks, wikis, and databases.",
-//       "url": "https://www.notion.so/"
-//   },
-//   {
-//       "title": "(3) Trees Logic Puzzle 53 Quiz - By Katie_Wandering",
-//       "url": "https://www.sporcle.com/games/Katie_Wandering/trees-logic-puzzle-53?playlist=more-of-katie_wanderings-trees-puzzles&creator=Katie_Wandering&pid=iN5fbe4b8"
-//   },
-//   {
-//       "title": "Archives - University of Victoria",
-//       "url": "https://web.uvic.ca/calendar2017-05/pdfs/undergraduate-201705_Part6.pdf"
-//   },
-//   {
-//       "title": "Log in to your account",
-//       "url": "https://accweb.mouv.desjardins.ca/identifiantunique/securite-garantie/authentification/auth/simple/0?domaineVirtuel=coast&executeTime=12"
-//   },
-//   {
-//       "title": "Google Docs",
-//       "url": "https://docs.google.com/"
-//   },
-//   {
-//       "title": "Homepage - University of Victoria",
-//       "url": "https://bright.uvic.ca/d2l/home"
-//   }
-// ]
+let devSites = [
+  {
+      "title": "Inbox - Gmail",
+      "url": "https://mail.google.com/",
+      "src": "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico"
+  },
+  {
+      "title": "zakwht (Zak)",
+      "url": "https://github.com/zakwht",
+      "src": "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+
+  },
+  {
+      "title": "Notion – The all-in-one workspace for your notes, tasks, wikis, and databases.",
+      "url": "https://www.notion.so/",
+      "src": "https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png"
+  },
+  {
+      "title": "Google Docs",
+      "url": "https://docs.google.com/",
+      "src": "https://cdn-icons-png.flaticon.com/512/5968/5968517.png"
+  },
+  {
+      "title": "zakwh1te • Instagram",
+      "url": "https://www.instagram.com/zakwh1te/",
+      "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"
+  }
+]
 
 function faviconURL(u: string) {
   const url = new URL(chrome.runtime.getURL("/_favicon/"));
@@ -62,24 +49,23 @@ export const TopSites = () => {
   const [topSites, setTopSites] = useState<TopSiteURL[]>([])
 
   useEffect(() => {
-    chrome.topSites.get().then(setTopSites)
+    if (process.env.NODE_ENV === "development")
+      setTopSites(devSites)
+    else chrome.topSites.get().then(setTopSites)
   }, [])
 
-  // console.log(topSites)
-  // console.log(process.env.NODE_ENV)
 
   return (
     <nav>
-      {topSites.slice(0,5).map(({title, url}) => (
+      {topSites.slice(0,5).map(({title, url, src}) => (
         <a
           key={url}
           href={url} target="_self">
             <figure>
               <img 
-              src={faviconURL(url)} 
-              alt="favicon"
-              width="24"
-              // src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico"
+                src={src || faviconURL(url)} 
+                alt="favicon"
+                width="24"
               />
             </figure>
           <label>{stripTitle(title)}</label>
