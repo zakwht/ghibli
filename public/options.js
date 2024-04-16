f68f9d4f2af2e670e5aaaf7565c056bbf87f43f4
Zak White <zakwht@gmail.com>
2024-04-15 17:49:45 -0700

    use localstorage for options (synchronous)

--- public/options.js
+++ public/options.js
@@ -2,32 +2,16 @@ const saveOptions = () => {
   let settings = Object.fromEntries(
     [...document.forms.settings.elements].map((x) => [x.id, x.checked])
   );
-
-  try {
-    chrome.storage.sync.set(settings);
-    window.close();
-  } catch (e) {
-    console.log(settings);
-  }
+  localStorage.setItem("ghibli-extension-settings", JSON.stringify(settings));
+  if (typeof chrome !== "undefined") window.close();
+  else console.log(settings);
 };
 
 const loadOptions = () => {
-  try {
-    chrome.storage.sync.get().then((settings) => {
-      [...document.forms.settings.elements].forEach((x) => {
-        if (x.type === "checkbox") x.checked = settings[x.id];
-      });
-    });
-  } catch (e) {
-    const settings = {
-      hideGGI: true,
-      showTopSites: true
-    };
-
-    [...document.forms.settings.elements].forEach((x) => {
-      if (x.type === "checkbox") x.checked = settings[x.id];
-    });
-  }
+  let settings = JSON.parse(localStorage.getItem("ghibli-extension-settings"));
+  [...document.forms.settings.elements].forEach((x) => {
+    if (x.type === "checkbox") x.checked = settings[x.id];
+  });
 };
 
 document.addEventListener("DOMContentLoaded", loadOptions);
